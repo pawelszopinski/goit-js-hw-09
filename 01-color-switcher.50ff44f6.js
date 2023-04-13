c4722830411a2ea8326cf9924a9a03f95354ce09
pawelszopinski <szopinskipawel@gmail.com>
2023-04-13 21:08:47 +0000

    Deploying to gh-pages from @ pawelszopinski/goit-js-hw-09@273760cb6942dbc4f78f8c1b5d8b2b3987342d95 🚀

---
+++ 01-color-switcher.50ff44f6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;e.disabled=!0,t.addEventListener("click",(()=>{d=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=t}),1e3,t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.50ff44f6.js.map
