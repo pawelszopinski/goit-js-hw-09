273760cb6942dbc4f78f8c1b5d8b2b3987342d95
Paweł Szopiński <szopinskipawel@gmail.com>
2023-04-13 23:08:53 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null
+stopBtn.disabled = true /* przycisk Stop na początku jest nieaktywny, bo nie ma żadnej roli do spełnienia */
 
+startBtn.addEventListener("click", ()=>{ 
+    intervalId = setInterval(()=>{
+        let randomColor = getRandomHexColor();
+        document.body.style.background = randomColor;
+    },1000,startBtn.disabled = true,stopBtn.disabled = false)
+    
+})
+stopBtn.addEventListener("click",()=>{
+    clearInterval(intervalId)
+    startBtn.disabled = false
+    stopBtn.disabled = true
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
